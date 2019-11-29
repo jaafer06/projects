6b7e51826d784ef601f805ca0063432b80c60b72
jaafar06 <jaafer06@hotmail.fr>
2019-11-29 14:00:35 +0100

    fixed numerical instabilities first stable version

--- voronoi_domains/internal/src/geometry.ts
+++ voronoi_domains/internal/src/geometry.ts
@@ -3,16 +3,24 @@ import { Path, Point } from "paper";
 
 export class Vector2D {
     public readonly asArray: [number, number];
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, draw: boolean = false) {
         this.asArray = [x, y]
+        if(draw) {
+            this.draw();
+        }
     }
 
+    
     isMultiple(other: Vector2D): boolean {
         return (this.x/other.x == this.y/other.y);
     }
 
     dot(other: Vector2D) {
         return this.asArray[0] * other.asArray[0] + this.asArray[1] * other.asArray[1];
+    }
+
+    dotRaw(x: number, y: number) {
+        return this.asArray[0] * x + this.asArray[1] * y;
     }
 
     get x() {
@@ -37,13 +45,19 @@ export class Vector2D {
         return new Vector2D(this.x, this.y);
     }
 
+    draw(color: string = "green") {
+        const d = new Path.Circle(new Point(this.asArray), 10);
+        d.fillColor = color;
+        return d;
+    }
+
 }
 
 export class HyperPlane {
     public readonly a: Vector2D;
     public readonly beta: number;
     public readonly angle: number;
-    constructor(x: number, y: number, beta: number) {
+    constructor(x: number, y: number, beta: number, draw: boolean = false) {
         const length = Math.sqrt(x*x+y*y);
         const xx = x/length;
         const yy = y/length;        
@@ -52,23 +66,23 @@ export class HyperPlane {
         this.beta = beta/length; 
 
         this.angle = Math.atan2(xx, yy);
+        if (draw) {
+            this.draw();
+        }
     }
 
-    public intersect(other: HyperPlane): Vector2D | undefined {
+    public intersect(other: HyperPlane): [number, number] | undefined {
         if(this.a.isMultiple(other.a)) {
             return undefined
         };
-        const result = (math.lusolve([other.a.asArray, this.a.asArray], [other.beta, this.beta]) as number[][]);
-        // let c = new Path.Circle(new Point(result[0][0], result[1][0]), 3);
-        // c.fillColor = "yellow";
 
-        return new Vector2D(result[0][0], result[1][0]);
+        const y = (other.beta - other.a.x * this.beta/this.a.x) / (other.a.y - other.a.x*this.a.y/this.a.x);
+        const x = (this.beta - y*this.a.y)/this.a.x
+        return [x, y];
     }
     
-    isInside(point: Vector2D) {
-        // console.log(point.dot(this.a), this.beta);
-        
-        return point.dot(this.a) <= this.beta+0.0005;
+    isInside(point: Vector2D) {        
+        return point.dot(this.a) <= this.beta+0.00005;
     }
 
     public draw() {
@@ -84,10 +98,13 @@ export class HyperPlane {
         const l = new Path.Line(new Point(v1.asArray), new Point(v2.asArray));
         l.strokeWidth = 3;
         l.strokeColor = "black";
+        const direction = new Path.Line(new Point(firstPoint.asArray), new Point(firstPoint.asArray).add(new Point(this.a.asArray).normalize(60)));
+        direction.strokeWidth = 3;
+        direction.strokeColor = "black";
+        return l;
     }
 
 }
-
 
 export class Line {
     from: Vector2D;
@@ -116,69 +133,126 @@ export function isInConvexHull(hyperplanes: HyperPlane[], point: Vector2D) {
     return true;
 }
 
+function isInConvexHullRaw(hyperplanes: HyperPlane[], x: number, y: number) {
+    for (let h of hyperplanes) {
+        if(!(x * h.a.x + y * h.a.y <= h.beta + 0.05)) {
+            return false;
+        };
+    }
+    return true;
+}
+
 function arrangeClockWise(hyperplanes: HyperPlane[]) {
     return hyperplanes.sort((h1, h2) => h1.angle < h2.angle? 1 :-1);
 }
 
-export function findFeasibleVertex(hyperplanes: HyperPlane[]) {
-    if(hyperplanes.length <3) {
-        return {h: hyperplanes[0], index: 0};
-    }
-    for(let i =1; i<hyperplanes.length-1; ++i) {
-        const hit = hyperplanes[i].intersect(hyperplanes[i-1]);
-        if(hit && isInConvexHull(hyperplanes, hit)) {
-            return {h: hyperplanes[i], index:i};
-        }
+//returns direction if hyperplane is active and not redundant, undefined otherwise
+function isSupporingHyperPlane(hyperplane: HyperPlane, hyperplanes: HyperPlane[], xp: number, yp: number): [number, number] | undefined {
 
-        const hit2 = hyperplanes[i].intersect(hyperplanes[i+1]);
-        if(hit2 && isInConvexHull(hyperplanes, hit2)) {
-            return {h: hyperplanes[i], index: i};
-        }
+    const x = -hyperplane.a.y;
+    const y = hyperplane.a.x; 
+
+    const xx = -x;
+    const yy = -y;
+    // new Vector2D(x+xp, y+yp).draw("blue");
+    // new Vector2D(xx+xp, yy+yp).draw("blue");
+    // new Vector2D(xp, yp).draw("yellow");
     
+    if(isInConvexHullRaw(hyperplanes, x+ xp, y +yp)) {
+        return [x, y];
+    } else if(isInConvexHullRaw(hyperplanes, xx +xp, yy + yp)) {
+        return [xx ,yy];
     }
-    console.log(hyperplanes);
-    
+    return undefined;
+}
+
+
+// returns hit, direction, none redundant active hyperplane, undefined otherwise
+export function findFeasibleVertex(hyperplanes: HyperPlane[]): [number, number, number, number, HyperPlane] {
+
+    for(let i =0; i<hyperplanes.length-1; ++i) {
+        for(let j=i+1; j<hyperplanes.length; ++j) {
+            const hit = hyperplanes[i].intersect(hyperplanes[j]);
+
+            if(hit && isInConvexHullRaw(hyperplanes, ...hit)) {
+                const direction = isSupporingHyperPlane(hyperplanes[i], hyperplanes, ...hit);
+                if(direction) {
+                    return [hit[0], hit[1], direction[0], direction[1], hyperplanes[i]];
+                }
+            }
+        }
+    }    
     throw "impossible";
 }
 
-export function convexHull(hyperplanes: HyperPlane[]) {
-    let convexRegion = [];
-    hyperplanes = arrangeClockWise(hyperplanes);
-    
-    const result = findFeasibleVertex(hyperplanes);
-    
-    let currentIndex = result.index;
-    let currentHyperPlaneForward = result.h;
-    while (currentIndex < hyperplanes.length-1) {
-        const r = currentHyperPlaneForward.intersect(hyperplanes[currentIndex+1]);
-        // console.log("forward" , r, isInConvexHull(hyperplanes, r as Vector2D));
-       
-        if(r && isInConvexHull(hyperplanes, r)) {
-            convexRegion.push(r);
-            currentHyperPlaneForward = hyperplanes[currentIndex+1];
-        }
-        ++currentIndex;
-    };
-    
-
-    currentIndex = result.index;
-    let currentHyperPlaneBackward = result.h;
-    while (currentIndex > 0) {
-        const r = currentHyperPlaneBackward.intersect(hyperplanes[currentIndex-1]);
-        // console.log("backward" , r, isInConvexHull(hyperplanes, r as Vector2D));
-        
-        if(r && isInConvexHull(hyperplanes, r)) {
-            convexRegion.unshift(r);
-            currentHyperPlaneBackward = hyperplanes[currentIndex-1];
-        }
-        --currentIndex;
-    };
-
-    const last = currentHyperPlaneBackward.intersect(currentHyperPlaneForward);
-    if(last) {
-        convexRegion.unshift(last);
+export function findDirection(hyperplanes: HyperPlane[], hyperplane: HyperPlane, xp: number, yp: number) {
+    const x = -hyperplane.a.y*10;
+    const y = hyperplane.a.x*10;
+    const xx = -x;
+    const yy = -y;
+    if(isInConvexHullRaw(hyperplanes, x+ xp, y +yp)) {
+        return [x, y];
+    } else if(isInConvexHullRaw(hyperplanes, xx +xp, yy + yp)) {
+        return [xx ,yy];
     }
-    return convexRegion;
+    return undefined;
+}
+
+// return: next point, next direction, actievHyperplane
+export function computeNexPoint(hyperplanes: HyperPlane[], startingHyperPlane: HyperPlane, x: number, y: number, xd: number, yd: number) {
+    let minDistance: number = Number.MAX_SAFE_INTEGER;
+    
+    let result: [number, number, number, number, HyperPlane] | undefined;
+    
+    for(let h of hyperplanes) {
+        if(h !== startingHyperPlane) {
+            const aTransposeDirection = h.a.dotRaw(xd, yd);
+            
+            if(aTransposeDirection == 0) {
+                continue;
+            }
+
+            const k = (h.beta-h.a.dotRaw(x, y)) / aTransposeDirection;
+            
+            if(k>=0.0005 && k < minDistance) {
+                const xNext = xd*k + x;
+                const yNext = yd*k + y;                
+                const newDirection = isSupporingHyperPlane(h, hyperplanes, xNext, yNext);                
+                if(newDirection) {
+                    result = [xNext, yNext, newDirection[0], newDirection[1], h];
+                }
+            }
+        }
+    }
+    
+    return result;
+}
+
+function drawPointAndDirection(x: number, y: number, xd: number, yd: number) {
+    new Vector2D(x+xd*10, y+yd*10).draw("blue");
+    new Vector2D(x, y).draw("yellow");
+
+}
+export function convexHull(hyperplanes: HyperPlane[], path: Path, color: string = "black") {
+    hyperplanes = arrangeClockWise(hyperplanes);
+    let [x, y, xd, yd, h] = findFeasibleVertex(hyperplanes);
+    const startingHyperplane = h;
+    path.removeSegments();
+    do {
+        path.add(new Point(x, y));
+        path.strokeColor = color;
+        path.strokeWidth = 2;
+        const result = computeNexPoint(hyperplanes, h, x, y, xd, yd);
+        if(result) {
+            [x, y, xd, yd, h] = result;
+        } else {
+            console.log("unexpected");
+            return;
+        }
+      
+    } while(startingHyperplane!= h);
+    path.closed = true;
+    
 }
 
 export function computeVoronoiDomains(points: Vector2D[]) {
@@ -197,6 +271,7 @@ export function computeVoronoiDomains(points: Vector2D[]) {
 function seperationHyperPlane(p1: Vector2D, p2: Vector2D) {
     const x = p2.x - p1.x;
     const y = p2.y - p1.y;
+    
     const beta = ((p2.x + p1.x)/2) * x + ((p2.y + p1.y)/2) * y;
-    return new HyperPlane(x,y, beta);
+    return new HyperPlane(x,y, beta, false);
 }
--- voronoi_domains/internal/src/main.ts
+++ voronoi_domains/internal/src/main.ts
@@ -5,18 +5,7 @@ import { disconnect } from "cluster";
 
 const rayLength: number = 600;
 
-function newLineFromPoint(p: Point, pp: Point) {
-
-    let l = new Path.Line(p.multiply(50), pp.multiply(50));
-    l.strokeWidth = 3;
-    l.strokeColor = "black";
-}
-
-function newLine(x: number, y: number, xx: number = 0, yy: number = 0) {
-    newLineFromPoint(new Point(x, y), new Point(xx, yy));
-}
-
-function getHyperplane(p1: Point, p2: Point, direction: -1|1) {
+function getHyperplane(p1: Point, p2: Point, direction: -1|1 = 1) {
     let x = -(p1.y - p2.y);
     let y = (p1.x - p2.x);
     if(p1.x*x+p1.y*y<0) {
@@ -25,11 +14,9 @@ function getHyperplane(p1: Point, p2: Point, direction: -1|1) {
     }
     
     let r = new geo.HyperPlane(x, y, (x*p1.x + y*p1.y))    
-    newLine(...r.a.asArray);
     r.draw();
     return r;
 }
-
 
 function createLineAndHyperPlane(p: Point, pp: Point) {
     hyperplanes.push(getHyperplane(p, pp, 1));
@@ -37,37 +24,38 @@ function createLineAndHyperPlane(p: Point, pp: Point) {
 
 let hyperplanes: geo.HyperPlane[] = [];
 let points: geo.Vector2D[] = [];
-
+let paths: Path[] = [];
 window.onload = function() {
     
     paper.setup("myCanvas");
     paper.view.center = new Point(0, 0);
     new Path.Circle(paper.view.center, 5).fillColor = "red";
-    let downPoint: Point;
+    const rect = paper.view.bounds;
 
-    // createLineAndHyperPlane(new Point(-133, -104), new Point(177, -67));
-    // createLineAndHyperPlane(new Point(124, -143), new Point(82, 145));
-    // createLineAndHyperPlane(new Point(-178, 114), new Point(144, 122));
-    // createLineAndHyperPlane(new Point(-114, 182), new Point(-102, -143));
-    // createLineAndHyperPlane(new Point(50, -136), new Point(135, 2));
+    hyperplanes.push(getHyperplane(rect.topRight, rect.topLeft));
+    hyperplanes.push(getHyperplane(rect.topRight, rect.bottomRight));
+    hyperplanes.push(getHyperplane(rect.bottomRight, rect.bottomLeft));
+    hyperplanes.push(getHyperplane(rect.bottomLeft, rect.topLeft));
+    // hyperplanes.push(new geo.HyperPlane(-0.8472419207998548, 0.5312072360570519, -99.95437422769716, true));
+    // hyperplanes.push(new geo.HyperPlane(0.7880243737245634, -0.6156440419723151, 149.94626286277708, true));
+    // hyperplanes.push(new geo.HyperPlane(-0.36476124982311886, -0.9311010850748034, 164.63211041358716, true));
+    // hyperplanes.push(new geo.HyperPlane(0.7979045642041152, -0.6027837974118424, 191.71312197222193, true));
+
+    paper.view.zoom = 1.0035;
 
 
     paper.view.onClick = (event: paper.MouseEvent) => {
        
         if(event.modifiers.alt) {
-            const result = geo.computeVoronoiDomains(points);
-            for(let rr of result) {
-                for(let r of rr) {
-                    r.draw();
-                }
-                const convexHull = geo.convexHull(rr);
-                for(let p of convexHull) {
-                    new Path.Circle(new Point(p.asArray), 5).fillColor = "blue";
-                }
-                break;
-               
+            if(paths.length != points.length) {
+                paths.forEach(p => p.remove());
+                paths = Array.from({length: points.length}, () => (new Path()));
             }
-            
+
+            const hyss = geo.computeVoronoiDomains(points);
+            for(let i =0; i< hyss.length; ++i) {
+                geo.convexHull(hyss[i].concat(...hyperplanes), paths[i] , "black");
+            }
             return;
         };
         
@@ -76,9 +64,12 @@ window.onload = function() {
             return;
         }
         
-        points.push(new geo.Vector2D(event.point.x, event.point.y));
-        new Path.Circle(event.point, 5).fillColor = "green";
+        points.push(new geo.Vector2D(event.point.x, event.point.y, true));
+        
     }
 
-    
+    paper.view.onMouseDrag = (event: paper.MouseEvent) => {
+        
+    }
+
 }
